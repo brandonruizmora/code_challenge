159748d2fb0f79fc60f612a1fcb09cee3a2d6d95
brandonruizmora <brandon_ruizmor@outlook.es>
2022-04-30 15:05:04 -0500

    Added test suit for Reader class and also added Reader class

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,12 @@
+const Reader = require("../../lib/utils/Reader");
+
+describe("Reader class unit test", () => {
+    test("Se esta leyendo el archivo correctamente", () => {
+        const students = Reader.readJSONFile("students.json");
+        expect(students).not.toBe(undefined);
+    });
+    test("Me trae 1 estudiante", () => {
+        const students = Reader.readJSONFile("students.json");
+        expect(students[0].email).toMatch(/visualpartnership/);
+    });
+});
