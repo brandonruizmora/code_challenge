1fd51b2d63887cd2d6ff5750934357dff3c2949a
brandonruizmora <brandon_ruizmor@outlook.es>
2022-04-30 15:08:01 -0500

    test to success to check github test automation test

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -3,7 +3,7 @@ const Reader = require("../../lib/utils/Reader");
 describe("Reader class unit test", () => {
     test("Se esta leyendo el archivo correctamente", () => {
         const students = Reader.readJSONFile("students.json");
-        expect(students).toBe(undefined);
+        expect(students).not.toBe(undefined);
     });
     test("Me trae 1 estudiante", () => {
         const students = Reader.readJSONFile("students.json");
