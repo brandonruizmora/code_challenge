8a8486bd23c646eec3bf4d4eec289d526b0287d1
brandonruizmora <brandon_ruizmor@outlook.es>
2022-04-30 15:48:16 -0500

    Added method getStudentsWithEnoughCredits

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -58,4 +58,34 @@ describe("StudentService class unit test", () => {
         expect(studentsList[0]).toMatch(/visualpartnership/);
         expect(studentsList[0]).toBe(students[0].email);
     });
+    test("Regresa una lista de estudiantes que tienen una certificación", () => {
+        const students = [{
+            "id": "6264d5d89f1df827eb84bb23",
+            "name": "Warren",
+            "email": "Todd@visualpartnership.xyz",
+            "credits": 500,
+            "enrollments": [
+                "Visual Thinking Intermedio",
+                "Visual Thinking Avanzado"
+            ],
+            "previousCourses": 1,
+            "haveCertification": true
+        },
+        {
+            "id": "6264d5d85cf81c496446b67f",
+            "name": "Lucinda",
+            "email": "Sexton@visualpartnership.xyz",
+            "credits": 501,
+            "enrollments": [
+                "Visual Thinking Avanzado"
+            ],
+            "previousCourses": 6,
+            "haveCertification": false
+        }];
+        const studentsList = StudentService.getStudentWithEnoughCreditsList(students);
+        expect(studentsList.length).toBe(1);
+        expect(studentsList[0].credits > 500).toBe(true);
+        expect(studentsList[0].credits).toBe(students[1].credits);
+        expect(studentsList[0]).toEqual(students[1]);
+    });
 });
