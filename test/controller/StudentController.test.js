5783ffbb1ee351dc44ef89d7b16eca0bc5dac198
brandonruizmora <brandon_ruizmor@outlook.es>
2022-04-30 16:17:18 -0500

    Added getStudentCreditList test & method

--- test/controller/StudentController.test.js
+++ test/controller/StudentController.test.js
@@ -7,9 +7,15 @@ describe("StudentService class unit test", () => {
         expect(typeof students).toBe(typeof []);
     });
     test("Regresa una lista de emails", () => {
-        const students = StudentController.getEmails();
+        const students = StudentController.getStudentEmailList();
         expect(typeof students).toBe(typeof []);
         expect(students.length).toBe(29);
         expect(students[0]).toMatch(/visualpartnership/);
+    });
+    test("Regresa una lista de estudiante con creditos suficientes", () => {
+        const students = StudentController.getStudentCreditList();
+        expect(typeof students).toBe(typeof []);
+        expect(students.length).toBe(27);
+        expect(students[0].credits > 500).toBeTruthy();
     });
 });
